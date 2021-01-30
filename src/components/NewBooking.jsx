1310d7f2c08f481d89c9ad456a55e5600655f38d
Nicole Young <coley504@gmail.com>
2021-01-30 23:24:19 +1100

    fixed syntax errors

--- src/components/NewBooking.jsx
+++ src/components/NewBooking.jsx
@@ -48,18 +48,3 @@ export function NewBooking() {
     </NewBookingForm>
   );
 }
-=======
-  <NewBookingForm>
-  <BookingLabel for="session">Session</BookingLabel>
-    <select name="session" id="session">
-      <option value ="Vinyasa Flow">Vinyasa Flow</option>
-      <option value ="Power Flow">Power Flow</option>
-      <option value ="Restorative Flow">Restorative Flow</option>
-    </select>
-    <BookingLabel  htmlFor="date">Date</BookingLabel>
-    <input type="text" name="date" id="date"/>
-    <BookingLabel htmlFor="Client Name">Client Name</BookingLabel>
-    <input type="text" name="Client Name" id="Client Name"/>
-    </NewBookingForm> 
-  )
-}
