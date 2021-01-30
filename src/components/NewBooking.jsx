69d238ef0b5fc52a58e4d9e18f231b9959217658
Nicole Young <coley504@gmail.com>
2021-01-30 21:35:20 +1100

    New Booking changes

--- src/components/NewBooking.jsx
+++ src/components/NewBooking.jsx
@@ -1,18 +1,48 @@
-import { NewBookingForm, BookingLabel} from '../styles/NewBooking'
+import React, { useState } from "react";
+import {
+  NewBookingForm,
+  BookingLabel,
+  BookingInput,
+  BookingInputSubmit,
+} from "../styles/NewBooking";
 
 export function NewBooking() {
+  const [session, setSession] = useState("");
+  const [date, setDate] = useState("");
+  const [clientName, setClientName] = useState("");
+
+  console.log(session, date, clientName)
+
   return (
-  <NewBookingForm>
-  <BookingLabel for="session">Session</BookingLabel>
-    <select name="session" id="session">
-      <option value ="Vinyasa Flow">Vinyasa Flow</option>
-      <option value ="Power Flow">Power Flow</option>
-      <option value ="Restorative Flow">Restorative Flow</option>
-    </select>
-    <BookingLabel  htmlFor="date">Date</BookingLabel>
-    <input type="text" name="date" id="date"/>
-    <BookingLabel htmlFor="Client Name">Client Name</BookingLabel>
-    <input type="text" name="Client Name" id="Client Name"/>
-    </NewBookingForm> 
-  )
+    <NewBookingForm>
+      <BookingLabel for="session">Session</BookingLabel>
+      <select
+        name="session"
+        id="session"
+        value={session}
+        onChange={(e) => setSession(e.target.val)}
+      >
+        <option value="Vinyasa Flow">Vinyasa Flow</option>
+        <option value="Power Flow">Power Flow</option>
+        <option value="Restorative Flow">Restorative Flow</option>
+      </select>
+      <BookingLabel htmlFor="date">Date</BookingLabel>
+      <BookingInput
+        type="text"
+        name="date"
+        id="date"
+        value={date}
+        onChange={(e) => setDate(e.target.val)}
+      />
+      <BookingLabel htmlFor="Client Name">Client Name</BookingLabel>
+      <BookingInput
+        type="text"
+        name="Client Name"
+        id="Client Name"
+        value={clientName}
+        onChange={(e) => setClientName(e.target.val)}
+      />
+      <BookingInputSubmit id="submit" type="submit" value="Submit" />
+    </NewBookingForm>
+  );
 }
