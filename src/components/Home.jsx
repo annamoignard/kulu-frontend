803627b388d28196a90abad23ec66d386eea1911
Anna Moignard <amoignard.coder@gmail.com>
2021-02-03 12:05:17 +1100

    styled some of homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,21 @@
+import React from 'react';
+import { HomeImg, KuluLogo, LogoContainer } from '../styles/Homepage'; 
+import kuluhome from '../assets/kuluhome.jpg';
+import kululogo from '../assets/kululogo.png';
+
+
 export function Home(){
   return ( 
-    <h1>Kulu</h1>
+    <>
+    <LogoContainer>
+      <KuluLogo>
+      <img src={kululogo} style={{ borderRadius: "50%" }} />
+      </KuluLogo>
+    </LogoContainer>
+
+    <HomeImg>
+      <img src={kuluhome} />
+    </HomeImg>
+    </>
     );
   }
