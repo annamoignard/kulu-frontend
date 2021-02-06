274c56165f3032117a67353bd8aa68703f8d796e
Nicole Young <coley504@gmail.com>
2021-02-06 17:16:46 +1100

    styling for booking page

--- src/styles/NewBooking.js
+++ src/styles/NewBooking.js
@@ -1,5 +1,7 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
 
+//Styles for form on NewBookings Page
 export const Form = styled.form`
 display: flex;
   flex-direction: column;
@@ -25,4 +27,32 @@ display: flex;
     height: 20px;
     margin: 1px 0px;
   }
+`;
+//Styles for Bookings Page
+export const Title = styled.h1`
+font-family: 'Poppins', sans-serif;
+text-align: center;`
+
+export const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  font-family: 'Poppins', sans-serif;
+`;
+
+export const OuterCard = styled.div`
+  width: 30%;
+`;
+
+export const InnerCard = styled.div`
+  margin: 10px 0px;
+  border: 1px solid black;
+  padding: 10px;
+  border-radius: 3px;
+`;
+
+export const CardLink = styled(Link)`
+  margin-right: 10px;
+  text-decoration: none;
+  color: crimson;
 `;
