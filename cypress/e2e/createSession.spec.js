bc524588600f9643aef0415fa4f822b3ae3f6d48
Nicole Young <coley504@gmail.com>
2021-02-09 09:53:00 +1100

    cypress tests fixup wip

--- cypress/e2e/createSession.spec.js
+++ cypress/e2e/createSession.spec.js
@@ -1,31 +1,37 @@
+describe("when clicking on login from the homepage user", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.findByText(/Login/).click();
+  });
+
+  it("should go to the login page", () => {
+    cy.url().should("include", "/login");
+  });
+
+  it("should see email and password inputs", () => {
+    cy.findByLabelText(/email/i).should("exist");
+    cy.findByLabelText(/password/i).should("exist");
+  });
+});
+
 describe("Create new Session", () => {
   it("should have a url /create-session", () => {
     cy.visit("/create-session");
     cy.url().should("include", "/create-session");
   });
 
-describe("when clicking on timetable from homepage user", () => {
-  beforeEach(() => {
-    cy.visit("/create-session");
-    cy.findByTestId(/login/).click();
+  describe("when clicking on timetable from homepage user", () => {
+    beforeEach(() => {
+      cy.visit("/create-session");
+      cy.findByTestId(/login/).click();
+    });
+
+    it("should be able to type into email and password inputs", () => {
+      const { email, password } = userBuilder();
+      cy.findByLabelText(/email/i).type(email).should("contain.value", email);
+      cy.findByLabelText(/password/i)
+        .type(password)
+        .should("contain.value", password);
+    });
   });
-
-  it("should be able to type into email and password inputs", () => {
-    const { email, password } = userBuilder()
-    cy.findByLabelText(/email/i).type(email).should("contain.value", email)
-    cy.findByLabelText(/password/i).type(password).should("contain.value", password)
-  })
-
-//   const sessionBuilder = build('Session', {
-//     fields: {
-//       name: fake((f) => f.name.findName()),
-//     }
-//   })
-  
-//   export {
-//     userBuilder,
-//     sessionBuilder
-//   }
-  
-});
 });
